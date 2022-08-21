7522f043babe09fb1d8a2de680058ff1a1eaf0f8
JoaoCioffi <joaorcioffi@gmail.com>
2022-08-21 18:15:37 -0300

    updated realTimeResponse module

---
+++ realTimeResponseModule/Scripts/main.py
@@ -0,0 +1,8 @@
+import dependencies
+import pandas as pd
+import numpy as np
+import colorama
+
+# Loading external modules:
+anomalyIdentifier = dependencies.findAnomaly()
+clusters = dependencies.clusters()
